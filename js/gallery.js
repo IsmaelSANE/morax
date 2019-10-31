36f792d83c696ea4068dd8f54586ae0dc2cb8c2a
Ismael Sane <six.echo.sa@gmail.com>
2019-10-31 15:42:57 +0100

    Clean up the code

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,7 @@
 // and respond appropriately on click.
 function activateGallery() {
   let thumbnails = document.querySelectorAll("#gallery-thumbs > div > img");
-  let mainImage = document.querySelector("#gallery-photo img");
+  let mainImage = document.querySelector("#gallery-photo > img");
 
   thumbnails.forEach(function(thumbnail) {
     thumbnail.addEventListener("click", function() {
